08b82a3cd149c9ff76fda1bfbad0d7082fe29ee1
Nasrudin Bin Salim <nasrudin.salim.suden@gmail.com>
2019-06-26 11:47:40 +0800

    "Autocommit from Nasrudin on 2019-06-26 00:00:00.000000"

--- autocommit.py
+++ autocommit.py
@@ -2,47 +2,50 @@ import subprocess
 import datetime
 import os
 
+
 USERNAME = subprocess.check_output('whoami', shell=True).decode('utf-8').strip('\n')
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 LOG_FOLDER = 'logs'
 TODAY_DATE = datetime.datetime.now().date()
 
-import argparse
+if __name__ == '__main__':
 
-cwd = os.getcwd()
+    import argparse
 
-parser = argparse.ArgumentParser(description="Get the working directory of where the repo was cloned into")
-parser.add_argument('cwd', type=str, nargs='?', const=cwd)
-args = parser.parse_args()
+    cwd = os.getcwd()
 
-cwd = args.cwd if args.cwd is not None else cwd
-os.chdir(cwd)
+    parser = argparse.ArgumentParser(description="Get the working directory of where the repo was cloned into")
+    parser.add_argument('cwd', type=str, nargs='?', const=cwd)
+    args = parser.parse_args()
 
-try:
-    os.mkdir(LOG_FOLDER )
-except:
-    pass
+    cwd = args.cwd if args.cwd is not None else cwd
+    os.chdir(cwd)
 
-incrementer_file_name = f'{USERNAME}.txt'
-incrementer_file_name = os.path.join(LOG_FOLDER, incrementer_file_name)
-new_user = False
-try:
-    last_run = subprocess.check_output(['tail', '-1', incrementer_file_name])
-except:
-    new_user = True
-if not new_user:
-    last_run_date = datetime.datetime.strptime(last_run.decode('utf-8').strip('\n'), DATE_FORMAT)
-    last_run_date_no_time = last_run_date.date()
+    try:
+        os.mkdir(LOG_FOLDER )
+    except:
+        pass
 
-    if last_run_date_no_time == TODAY_DATE:
-        exit()
+    incrementer_file_name = f'{USERNAME}.txt'
+    incrementer_file_name = os.path.join(LOG_FOLDER, incrementer_file_name)
+    new_user = False
+    try:
+        last_run = subprocess.check_output(['tail', '-1', incrementer_file_name])
+    except:
+        new_user = True
+    if not new_user:
+        last_run_date = datetime.datetime.strptime(last_run.decode('utf-8').strip('\n'), DATE_FORMAT)
+        last_run_date_no_time = last_run_date.date()
 
-with open(incrementer_file_name, 'a') as f:
-    f.write(TODAY_DATE.strftime(DATE_FORMAT))
+        if last_run_date_no_time == TODAY_DATE:
+            exit()
 
-subprocess.run(['git', 'add', '-A'])
-subprocess.run(['git', 'commit', '-m', f'"Autocommit from {USERNAME} on {TODAY_DATE.strftime(DATE_FORMAT)}"'])
-subprocess.run(['git', 'fetch'])
-subprocess.run(['git', 'rebase', 'origin/master'])
-subprocess.run(['git', 'push', '-f'])
+    with open(incrementer_file_name, 'a') as f:
+        f.write(TODAY_DATE.strftime(DATE_FORMAT))
+
+    subprocess.run(['git', 'add', '-A'])
+    subprocess.run(['git', 'commit', '-m', f'"Autocommit from {USERNAME} on {TODAY_DATE.strftime(DATE_FORMAT)}"'])
+    subprocess.run(['git', 'fetch'])
+    subprocess.run(['git', 'rebase', 'origin/master'])
+    subprocess.run(['git', 'push', '-f'])
 
